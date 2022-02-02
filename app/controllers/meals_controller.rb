c88cef88182e2f6a5dee99669c0c675642bbd3fe
qdtrl <quentindtouroul@gmail.com>
2022-02-02 23:27:01 +0100

    config mine

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,7 +1,6 @@
 class MealsController < ApplicationController
   def index
     @meals = Meal.all
-
     render json: @meals
   end
   
@@ -11,7 +10,6 @@ class MealsController < ApplicationController
     render json: { meal: @meal, ingredients: @meal.ingredients }
   end
   
-    
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_meal
