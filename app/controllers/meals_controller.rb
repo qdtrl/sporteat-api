67543cb9abb70750b07528a0d2e2e825fbf7e546
qdtrl <quentindtouroul@gmail.com>
2022-02-03 13:03:32 +0100

    c

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -11,8 +11,8 @@ class MealsController < ApplicationController
   end
   
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_meal
-      @meal = Meal.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_meal
+    @meal = Meal.find(params[:id])
+  end
 end
